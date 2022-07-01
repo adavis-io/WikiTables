cfa7f76e642c3120aec83a52439f3f5e98976dc5
adavis-io <65918022+adavis-io@users.noreply.github.com>
2022-06-30 23:37:38 -0700

    Added documentation

--- clipboardtables.py
+++ clipboardtables.py
@@ -1,6 +1,18 @@
 from tkinter import Tk
 
 def build_table(clipboard, add_header=True):
+    '''Generate a list of wiki-formatted table rows from a string provided,
+    optionally including a template header row. Assumes input string data is
+    tab-delimited - i.e. tabs delimit columns of data, with newlines separating
+    individual rows.
+
+    Arguments: 
+        clipboard: a string containing the table to format
+        add_header: whether to add a template header row
+
+    returns: 
+        a list containing wiki-formatted table rows as strings 
+    '''
     if not clipboard:
         return
     
@@ -19,6 +31,13 @@ def build_table(clipboard, add_header=True):
     return table_rows
 
 if __name__ == '__main__':
+    '''Generate a Wiki-formatted text table of the clipboard contents.
+
+    Usage: Enter data into Excel as normal. Highlight the data table to format
+    and copy to the clipboard. Run 'py clipboardtables.py' from command prompt,
+    PowerShell, or terminal of choice, then copy the string output for later
+    use.
+    '''
     clipboard = Tk().clipboard_get()
     table = build_table(clipboard)
     
